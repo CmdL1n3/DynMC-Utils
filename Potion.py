e2fcda6b1c699c806b73d2b75ca6ee15560f089f
CmdBlk <evanbombtheredstoner@gmail.com>
2015-07-28 23:46:12 +0100

    Added slowness

--- Potion.py
+++ Potion.py
@@ -3,8 +3,9 @@ import org.bukkit.potion.PotionEffect as PotionEffect
 import org.bukkit.potion.PotionEffectType as PotionEffectType
 import org.bukkit.event.player.PlayerJoinEvent as PlayerJoinEvent
 
-jump_perm = "utils.jump"
+jump_perm  = "utils.jump"
 speed_perm = "utils.speed"
+slow_perm  = "utils.slow"
 
 #Jump boost
 @hook.event("player.PlayerJoinEvent", "high")
@@ -19,3 +20,10 @@ def on_join(event):
     if event.getPlayer().hasPermission(speed_perm):
       player = event.getPlayer()	  
       player.addPotionEffect(PotionEffect(PotionEffectType.SPEED, 60000, 0))
+
+#Slowness	  
+@hook.event("player.PlayerJoinEvent", "high")
+def on_join(event):
+    if event.getPlayer().hasPermission(slow_perm):
+      player = event.getPlayer()	  
+      player.addPotionEffect(PotionEffect(PotionEffectType.SLOWNESS, 60000, 0))
