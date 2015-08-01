4f56deef6396f0db21331ff6832956160b0e054e
CmdBlk <rythian.morg@hotmail.com>
2015-08-01 15:05:01 +0100

    uhh..
    uhh...

--- teleporter.py
+++ teleporter.py
@@ -1,29 +1,43 @@
 from helpers import *
-import org.bukkit.potion.PotionEffect as PotionEffect
-import org.bukkit.potion.PotionEffectType as PotionEffectType
-import org.bukkit.event.player.PlayerJoinEvent as PlayerJoinEvent
 
-jump_perm  = "utils.jump"
-speed_perm = "utils.speed"
-slow_perm  = "utils.slow"
 
-#Jump boost
-@hook.event("player.PlayerJoinEvent", "high")
-def on_join(event):
-    if event.getPlayer().hasPermission(jump_perm):
-      player = event.getPlayer()		  
-      player.addPotionEffect(PotionEffect(PotionEffectType.JUMP, 60000, 0))
-	  
-#Speed	  
-@hook.event("player.PlayerJoinEvent", "high")
-def on_join(event):
-    if event.getPlayer().hasPermission(speed_perm):
-      player = event.getPlayer()	  
-      player.addPotionEffect(PotionEffect(PotionEffectType.SPEED, 60000, 0))
 
-#Slowness	  
-@hook.event("player.PlayerJoinEvent", "high")
-def on_join(event):
-    if event.getPlayer().hasPermission(slow_perm):
-      player = event.getPlayer()	  
-      player.addPotionEffect(PotionEffect(PotionEffectType.SLOWNESS, 60000, 0))
+prefix = "&a-=[&cteleporter&a]=-&e"
+
+
+"""   Command handling:    """
+
+#set teleporter locations
+def set_pos(self, sender, type):
+    loc = sender.getLocation()
+	if args[1] == "pos":
+		if args[0] == "co1":
+			self.pos1 = Coords(loc)
+			msg(sender, "%s position 2 set!" % prefix)
+		elif args[0] == "co2":
+			self.pos2 = Coords(loc)
+			msg(sender, "%s position 2 set!" % prefix)
+		
+@hook.event("player.PlayerMoveEvent", "high")
+def onMove(event):
+	if player.getLocation() == self.pos1:
+		player.teleport(self.pos2)
+		
+		
+		
+		
+		
+		
+		
+"""  Command setup   """
+
+@hook.command("teleporter")
+def on_teleporter_command(sender, command, label, args):
+	if args < 1
+		msg(sender, "%s Info:" % prefix)
+		msg(sender, "&e /teleporter pos <co1|co2>")
+		msg(sender, "&e Sets the positions for a teleporter")
+		msg(sender, "&e")
+		msg(sender, "&e more to come")
+	
+    return set_pos(self, sender, type)
