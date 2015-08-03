858bab15da52132d86a853a4132040ea7d9e9e4e
CmdBlk <rythian.morg@hotmail.com>
2015-08-03 16:35:08 +0100

    Bugfixes, still not working teleporter but nearly done, bye bye potion, you were not needed

--- teleporter.py
+++ teleporter.py
@@ -1,50 +1,50 @@
 from helpers import *
-from basecommands import simplecommand
 
+pos1 = None
+pos2 = None
 
-prefix = "&a-=[&cteleporter&a]=-&e"
-
-
-"""   Command handling:    """
-
-#set teleporter locations
-def set_pos(self, sender, type):
-	if args[1] == "pos":
-	  loc = sender.getLocation()
-	  if args[2] in ["co1"]:
-			self.pos1 = Coords(loc)
-			msg(sender, "%s position 2 set!" % prefix)
-	  elif args[2] in ["co2"]:
-			self.pos2 = Coords(loc)
-			msg(sender, "%s position 2 set!" % prefix)
-		
-@hook.event("player.PlayerMoveEvent", "high")
-def onMove(event):
-	if player.getLocation() == self.pos1:
-		player.teleport(self.pos2)
-		
-		
-		
-		
-		
-		
-		
-"""  Command setup   """
+def help(sender):
+	msg(sender," \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
+	msg(sender,"&a \n \n \n \n-=[&6Teleporter&a]=- \n \n \n \n \n")
+	msg(sender,"&6/teleporter pos <pos1|pos2>")
+	msg(sender,"&6Sets points for a teleporter.")
+	
+	
+def noArgs(sender):
+	msg(sender," \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")
+	msg(sender,"&cLooks like you supplied no args!")
+	msg(sender,"&cIf you need help, use /teleporter help!")
+	msg(sender," \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n")	
 
 @simplecommand("teleporter",
-        aliases = ["tpr"], 
-        usage = "pos <co1|co2>", 
-        description = "Creates teleporters",
-        senderLimit = 0,
-        helpNoargs = True,
-        helpSubcmd = True,
-        amax = 2)
-def on_teleporter_command(sender, command, label, args):
-	if args < 1:
-		msg(sender, "%s Info:" % prefix)
-		msg(sender, "&e /teleporter pos <co1|co2>")
-		msg(sender, "&e Sets the positions for a teleporter")
-		msg(sender, "&e")
-		msg(sender, "&e more to come")
+	usage       = "<pos> <pos1|pos2>",
+	description = "creates teleporters",
+	senderLimit = 0,
+	helpNoargs  = False)
+def onTeleporterCommand(sender, command):
+	if args[0] == "pos":
+		if args[1] == "pos1":
+			pos1 = sender.getLocation()
+			msg(sender,"&6Position 1 saved!")		
+		elif args[1] == "pos2":
+			pos2 = sender.getLocation()
+			msg(sender,"&6Position 2 saved!")
+		else:
+			noArgs()
+	elif args[0] == "help":
+		help()	
+	else:
+		noArgs()
+		
+		
+@hook.command("tpr")
+def onTeleporterCommand(sender, command):
+	return onTeleporterCommand(sender, command)
 	
-	return set_pos(self, sender, type)
+@hook.event("player.PlayerMoveEvent", "high")
+def onMove(event):
+	loc1 = player.getLocation()
+	if not int(pos1.x) == int(loc1x) and int(pos1.y) == int(loc1.y) and int(pos1.z) == int(loc1.z) and pos1.world.getName() == loc1.world.getName():
+		return
+	else:	
+		safetp(sender, pos2.getWorld(), pos2.x, pos2.y, pos2.z)
